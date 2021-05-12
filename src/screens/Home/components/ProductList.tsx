ea685a1a00a259986ec53d74ca829c1b1e3e7b3c
Japhetism <babatundeojo30@gmail.com>
2021-05-12 17:37:14 +0100

    add mobile filter

--- src/screens/Home/components/ProductList.tsx
+++ src/screens/Home/components/ProductList.tsx
@@ -16,13 +16,13 @@ const ProductList: React.FC<ProductListProps> = ({ products , globalProducts}) =
         <>
             <Container {...{className: "container"}}>
                 <Row>
-                    <Column {...{className: "col-lg-10 col-8"}}>
+                    <Column {...{className: "col-lg-10 col-11"}}>
                         <H5 {...{className: "product-title"}}>
                             <Span {...{className: "black-color"}}>Photography /</Span>
                             <Span {...{className: "grey-color"}}> Premium Photos</Span>
                         </H5>
                     </Column>
-                    <Column {...{className: "col-lg-2 col-4 float-content-right text-right"}}>
+                    <Column {...{className: "col-lg-2 col-4 float-content-right text-right web-filter-container"}}>
                         <Span {...{className: "grey-color sort"}}>
                             <svg width="7" height="15" viewBox="0 0 7 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.64807 14.3734V1.5347L5.90435 3.79098C5.97793 3.86456 6.07296 3.90134 6.17106 3.90134C6.26916 3.90134 6.36419 3.86456 6.43777 3.79098C6.58492 3.64383 6.58492 3.40778 6.43777 3.26063L3.54077 0.363637C3.39976 0.222619 3.15144 0.222619 3.01042 0.363637L0.110362 3.26063C-0.0367873 3.40778 -0.0367873 3.64383 0.110362 3.79098C0.257511 3.93813 0.493562 3.93813 0.640711 3.79098L2.897 1.5347V14.3734C2.897 14.5819 3.0656 14.7505 3.27407 14.7505C3.47946 14.7474 3.64807 14.5788 3.64807 14.3734Z" fill="black"/>
@@ -41,9 +41,57 @@ const ProductList: React.FC<ProductListProps> = ({ products , globalProducts}) =
                             </svg>
                         </Span>
                     </Column>
+                    <Column {...{className: "col-lg-2 col-1 float-content-right text-right mobile-filter-container"}}>
+                        <Container {...{className: "filter-icon"}}>
+                            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	                            viewBox="0 0 512 512" xmlSpace="preserve"
+                            >
+                                <g>
+                                    <g>
+                                        <path d="M490.667,405.333h-56.811C424.619,374.592,396.373,352,362.667,352s-61.931,22.592-71.189,53.333H21.333
+                                            C9.557,405.333,0,414.891,0,426.667S9.557,448,21.333,448h270.144c9.237,30.741,37.483,53.333,71.189,53.333
+                                            s61.931-22.592,71.189-53.333h56.811c11.797,0,21.333-9.557,21.333-21.333S502.464,405.333,490.667,405.333z M362.667,458.667
+                                            c-17.643,0-32-14.357-32-32s14.357-32,32-32s32,14.357,32,32S380.309,458.667,362.667,458.667z"/>
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <path d="M490.667,64h-56.811c-9.259-30.741-37.483-53.333-71.189-53.333S300.736,33.259,291.477,64H21.333
+                                            C9.557,64,0,73.557,0,85.333s9.557,21.333,21.333,21.333h270.144C300.736,137.408,328.96,160,362.667,160
+                                            s61.931-22.592,71.189-53.333h56.811c11.797,0,21.333-9.557,21.333-21.333S502.464,64,490.667,64z M362.667,117.333
+                                            c-17.643,0-32-14.357-32-32c0-17.643,14.357-32,32-32s32,14.357,32,32C394.667,102.976,380.309,117.333,362.667,117.333z"/>
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <path d="M490.667,234.667H220.523c-9.259-30.741-37.483-53.333-71.189-53.333s-61.931,22.592-71.189,53.333H21.333
+                                            C9.557,234.667,0,244.224,0,256c0,11.776,9.557,21.333,21.333,21.333h56.811c9.259,30.741,37.483,53.333,71.189,53.333
+                                            s61.931-22.592,71.189-53.333h270.144c11.797,0,21.333-9.557,21.333-21.333C512,244.224,502.464,234.667,490.667,234.667z
+                                            M149.333,288c-17.643,0-32-14.357-32-32s14.357-32,32-32c17.643,0,32,14.357,32,32S166.976,288,149.333,288z"/>
+                                    </g>
+                                </g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                                <g></g>
+                            </svg>
+                        </Container>
+
+                    </Column>
                 </Row>
                 <Row>
-                    <Column {...{className: "col-lg-3"}}>
+                    <Column {...{className: "col-lg-3 product-price-category-container"}}>
                         <ProductCategory />    
                         <PriceRange />
                     </Column>
